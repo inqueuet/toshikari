62d1d5cdb2d244cc1ddafeddd2fbff020fccb5fa
admin <admin@inqueuet.com>
2026-03-18 00:03:44 +0900

    保守性向上

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.valoser.toshikari"
         minSdk = 24
         targetSdk = 36 // Android API 36
-        versionCode = 6 // 内部バージョン（Play Console 配信管理で使用）
-        versionName = "1.1" // 表示バージョン
+        versionCode = 11 // 内部バージョン（Play Console 配信管理で使用）
+        versionName = "1.6" // 表示バージョン
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
